dffdb69970b0d833eaa25d4451460feb428813db
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-25 17:39:09 +1300

    chore: add sidebar open/close animation

--- frontend/src/layouts/sidebar/index.js
+++ frontend/src/layouts/sidebar/index.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Sidebar = () => {
+  const SidebarAnimation = {
+    // System view
+    open: {
+      width: "16rem",
+      transition: {
+        damping: 40,
+      },
+    },
+    closed: {
+      width: "4rem",
+      transition: {
+        damping: 40,
+      },
+    },
+  };
+
   return (
     <div>
-      <div className="bg-white text-gray shadow-xl z-[999] w-[16rem] max-w-[16rem] h-screen overflow-hidden md:relative fixed"></div>
+      <motion.div
+        variants={SidebarAnimation}
+        animate={"open"}
+        className="bg-white text-gray shadow-xl z-[999] w-[16rem] max-w-[16rem] h-screen overflow-hidden md:relative fixed"
+      ></motion.div>
     </div>
   );
 };
