01a0ec2b4553ac3fbb69e322fee9cda235cce1b2
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-25 20:30:26 +1300

    chore: add open/close submenu animation

--- frontend/src/layouts/sidebar/SubMenu.js
+++ frontend/src/layouts/sidebar/SubMenu.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 import { IoIosArrowDown } from "react-icons/io";
 import { NavLink, useLocation } from "react-router-dom";
 
 const SubMenu = ({ data }) => {
   const { pathname } = useLocation();
+  const [subMenuOpen, setSubMenuOpen] = useState(false);
+
   return (
     <>
-      <li className={`link ${pathname.includes(data.name) && "text-blue-600"}`}>
+      <li
+        className={`link ${pathname.includes(data.name) && "text-blue-600"}`}
+        onClick={() => setSubMenuOpen(!subMenuOpen)}
+      >
         <data.icon size={23} className="min-w-max" />
         <p className="capitalize flex-1">{data.name}</p>
         <IoIosArrowDown className="" />
       </li>
-      <ul className="flex flex-col pl-14 text=[0.8rem] font-normal overflow-hidden">
+      <motion.ul
+        animate={
+          subMenuOpen
+            ? {
+                height: "fit-content",
+              }
+            : {
+                height: 0,
+              }
+        }
+        className="flex flex-col pl-14 text=[0.8rem] font-normal overflow-hidden h-0"
+      >
         {data.menus.map((menu) => (
           <li key={menu}>
             {/* 
@@ -26,7 +43,7 @@ const SubMenu = ({ data }) => {
             </NavLink>
           </li>
         ))}
-      </ul>
+      </motion.ul>
     </>
   );
 };
