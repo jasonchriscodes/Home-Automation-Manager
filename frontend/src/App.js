2902e3047aa859edec6ce161a4b3de7c75db4e50
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-25 17:12:11 +1300

    chore: add sidebar routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
 import Account from "./pages/Account";
 import Analytics from "./pages/Analytics";
 import Devices from "./pages/Devices";
@@ -10,13 +11,15 @@ import Settings from "./pages/Settings";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/devices/:deviceId" element={<Devices />} />
-        <Route path="/analytics:analyticId" element={<Analytics />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
+      <RootLayout>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/devices/:deviceId" element={<Devices />} />
+          <Route path="/analytics:analyticId" element={<Analytics />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </RootLayout>
     </BrowserRouter>
   );
 }
---
+++ frontend/src/layouts/sidebar/SubMenu.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SubMenu = () => {
+  return <div>SubMenu</div>;
+};
+
+export default SubMenu;
