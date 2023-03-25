a76008ad5c60aa17b6d4a7d5fe852c14b2d46c70
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-25 20:13:02 +1300

    chore: add indicator on active path

--- frontend/src/layouts/sidebar/SubMenu.js
+++ frontend/src/layouts/sidebar/SubMenu.js
@@ -3,9 +3,10 @@ import { IoIosArrowDown } from "react-icons/io";
 import { NavLink, useLocation } from "react-router-dom";
 
 const SubMenu = ({ data }) => {
+  const { pathname } = useLocation();
   return (
     <>
-      <li className="link">
+      <li className={`link ${pathname.includes(data.name) && "text-blue-600"}`}>
         <data.icon size={23} />
         <p className="capitalize flex-1">{data.name}</p>
         <IoIosArrowDown className="" />
