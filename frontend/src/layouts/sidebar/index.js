65fc5c6ccef460ad7d85aa44449a52618e555bb3
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-25 17:19:45 +1300

    chore: add sidebar styling

--- frontend/src/layouts/sidebar/index.js
+++ frontend/src/layouts/sidebar/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <div className="bg-white text-gray shadow-xl z-[999] w-[16rem] max-w-[16rem] h-screen overflow-hidden md:relative fixed"></div>
+    </div>
+  );
 };
 
 export default Sidebar;
