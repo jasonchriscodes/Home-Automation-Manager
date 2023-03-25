ffbdb602a238c41ff3613370d13a02d851f3618f
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-25 18:43:22 +1300

    chore: add collapse button animation

--- frontend/src/layouts/sidebar/index.js
+++ frontend/src/layouts/sidebar/index.js
@@ -28,14 +28,30 @@ const Sidebar = () => {
         animate={isOpen ? "open" : "closed"}
         className="bg-white text-gray shadow-xl z-[999] w-[16rem] max-w-[16rem] h-screen overflow-hidden md:relative fixed"
       >
-        <div
+        <motion.div
+          animate={
+            isOpen
+              ? {
+                  x: 0,
+                  y: 0,
+                  rotate: 0,
+                }
+              : {
+                  x: -10,
+                  y: -200,
+                  rotate: 180,
+                }
+          }
+          transition={{
+            duration: 0,
+          }}
           onClick={() => setIsOpen(!isOpen)}
           className="absolute w-fit h-fit z-50 right-2 bottom-5 cursor-pointer
           border-dark-purple border-2 rounded-full hover:bg-blue-100 md:block hidden
           "
         >
           <IoIosArrowBack size={25} />
-        </div>
+        </motion.div>
       </motion.div>
     </div>
   );
