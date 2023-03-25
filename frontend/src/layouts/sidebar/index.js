93640d048f4d5195da32bed8e8e476087e908cbf
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-25 17:43:36 +1300

    chore: add state to sidebar animation

--- frontend/src/layouts/sidebar/index.js
+++ frontend/src/layouts/sidebar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 const Sidebar = () => {
@@ -18,11 +18,13 @@ const Sidebar = () => {
     },
   };
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
       <motion.div
         variants={SidebarAnimation}
-        animate={"open"}
+        animate={isOpen ? "open" : "closed"}
         className="bg-white text-gray shadow-xl z-[999] w-[16rem] max-w-[16rem] h-screen overflow-hidden md:relative fixed"
       ></motion.div>
     </div>
