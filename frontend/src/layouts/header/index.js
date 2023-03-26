de88d0ce99c6587f15ece04eea64f1f025684fd0
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-26 17:32:15 +1300

    chore: add drop down panel in messages

--- frontend/src/layouts/header/index.js
+++ frontend/src/layouts/header/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Fragment } from "react";
 import {
   HiOutlineBell,
   HiOutlineChatAlt,
   HiOutlineSearch,
 } from "react-icons/hi";
-import { Popover } from "@headlessui/react";
+import { Popover, Transition } from "@headlessui/react";
+import { classNames } from "classnames";
 
 const index = ({ children }) => {
   return (
@@ -24,9 +25,34 @@ const index = ({ children }) => {
         <Popover className="relative">
           {({ open }) => (
             <>
-              <Popover.Button className="p-1.5 rounded-sm inline-flex items-center text-gray-700 hover:text-opacity-100 focus:outline-none active:bg-gray-100">
+              <Popover.Button
+                className={classNames(
+                  open && "bg-gray-100",
+                  "p-1.5 rounded-sm inline-flex items-center text-gray-700 hover:text-opacity-100 focus:outline-none active:bg-gray-100"
+                )}
+              >
                 <HiOutlineChatAlt fontSize={24} />
               </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <Popover.Panel className="absolute right-0 z-10 mt-2.5 w-80">
+                  <div className="bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5">
+                    <strong className="text-gray-700 font-medium">
+                      Messages
+                    </strong>
+                    <div className="mt-2 py-1 text-sm">
+                      This is messages panel.
+                    </div>
+                  </div>
+                </Popover.Panel>
+              </Transition>
             </>
           )}
         </Popover>
