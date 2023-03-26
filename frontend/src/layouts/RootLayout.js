dc44544f81479d85953ecd5510c54e7b123893d1
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-26 14:30:24 +1300

    chore: add header component

--- frontend/src/layouts/RootLayout.js
+++ frontend/src/layouts/RootLayout.js
@@ -1,10 +1,14 @@
 import Sidebar from "./sidebar";
+import Header from "./header";
 
 function RootLayout({ children }) {
   return (
     <div className="flex gap-5">
       <Sidebar />
-      <main className="max-w-5xl flex-1 mx-auto py-4">{children}</main>
+      <div className="flex-1">
+        <Header />
+        <main className="max-w-5xl flex-1 mx-auto py-4">{children}</main>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/layouts/header/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>Header</div>;
+};
+
+export default index;
