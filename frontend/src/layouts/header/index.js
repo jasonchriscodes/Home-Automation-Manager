fe54a07e71f28b2eb22530e7a0188c5d8214ad96
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-26 16:41:25 +1300

    chore: add search bar

--- frontend/src/layouts/header/index.js
+++ frontend/src/layouts/header/index.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { HiOutlineSearch } from "react-icons/hi";
 
 const index = ({ children }) => {
   return (
-    <div className="bg-white h-20 px-4">
-      {/* <div>Search field</div> */}
-      <div className="max-w-5xl flex-1 mx-auto py-4">{children}</div>
+    <div className="bg-white h-20 px-4 flex justify-between items-center border-b border-gray-200">
+      <div className="relative">
+        <HiOutlineSearch
+          fontSize={20}
+          className="text-gray-400 absolute top-1/2 -translate-y-1/2 left-3"
+        />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="text-sm focus:outline-none active:outline-none h-10 w-[24rem] border border-gray-300 rounded-md pr-4 pl-11"
+        />
+      </div>
+      <div>Side button</div>
+      {/* <div className="max-w-5xl flex-1 mx-auto py-4">{children}</div> */}
     </div>
   );
 };
