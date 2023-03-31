b5a60a9b070f1749863620f9a0d794fb3794febe
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-04-01 00:24:48 +1300

    chore: add notification panel

--- frontend/src/layouts/header/index.js
+++ frontend/src/layouts/header/index.js
@@ -46,7 +46,40 @@ const index = ({ children }) => {
             </>
           )}
         </Popover>
-        <HiOutlineBell fontSize={24} />
+        <Popover className="relative">
+          {({ open }) => (
+            <>
+              <Popover.Button
+                className={classNames(
+                  open && "bg-gray-100",
+                  "p-1.5 rounded-sm inline-flex items-center text-gray-700 hover:text-opacity-100 focus:outline-none active:bg-gray-100"
+                )}
+              >
+                <HiOutlineBell fontSize={24} />
+              </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <Popover.Panel className="absolute right-0 z-10 mt-2.5 w-80">
+                  <div className="bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5">
+                    <strong className="text-gray-700 font-medium">
+                      Notification
+                    </strong>
+                    <div className="mt-2 py-1 text-sm">
+                      This is notification panel.
+                    </div>
+                  </div>
+                </Popover.Panel>
+              </Transition>
+            </>
+          )}
+        </Popover>
       </div>
     </div>
   );
