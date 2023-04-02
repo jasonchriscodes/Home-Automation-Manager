1db2a8ae76b7657596968c4a9cb9d339659c5ee6
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-04-02 16:35:10 +1200

    chore: add room floor map

--- frontend/src/pages/Rooms.js
+++ frontend/src/pages/Rooms.js
@@ -1,9 +1,51 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
+import VectorMap, { Layer, Tooltip, Label } from "devextreme-react/vector-map";
+
+import { roomsData, buildingData } from "./../constants";
+
+const projection = {
+  to: ([l, lt]) => [l / 100, lt / 100],
+  from: ([x, y]) => [x * 100, y * 100],
+};
+
 const Rooms = () => {
+  function customizeTooltip(arg) {
+    if (arg.layer.name === "rooms") {
+      return {
+        text: `Devices On: ${arg.attribute("device")} / ${arg.attribute(
+          "totalDevices"
+        )}`,
+      };
+    }
+    return null;
+  }
+
   return (
-    <div className="h-screen mx-12">
+    <div className="h-screen mx-5">
+      <div className="flex justify-center p-6">
+        <VectorMap
+          className="h-1/2 w-full"
+          maxZoomFactor={4}
+          projection={projection}
+        >
+          <Layer
+            dataSource={buildingData}
+            hoverEnabled={false}
+            name="building"
+          ></Layer>
+          <Layer
+            dataSource={roomsData}
+            name="rooms"
+            borderWidth={1}
+            color="transparent"
+          >
+            <Label enabled={true} dataField="name"></Label>
+          </Layer>
+          <Tooltip enabled={true} customizeTooltip={customizeTooltip}></Tooltip>
+        </VectorMap>
+      </div>
       <div className="flex justify-center gap-3 mx-3 p-5">
         <NavLink className="hover:text-blue-400" to="bedroom">
           Bedroom
