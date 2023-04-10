29b3d8c88c2bd3c74475066728919179721bbc86
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-04-10 21:36:22 +1200

    chore: add watering system

--- frontend/src/pages/devices/Watering.js
+++ frontend/src/pages/devices/Watering.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import Switch from "./../../components/Switch";
 
 const Watering = () => {
-  return <div>Watering</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const buttonText = isOpen ? "Stop" : "Water";
+  const gradientStart = isOpen
+    ? "from-red-500 via-red-600 bg-pos-0"
+    : "from-blue-800 via-blue-600 bg-pos-0";
+  const gradientEnd = isOpen
+    ? "to-red-400 bg-pos-100"
+    : "to-blue-400 bg-pos-100";
+
+  return (
+    <div className="p-4 shadow-md flex flex-wrap flex-col justify-between bg-white">
+      <div className="flex-col">
+        <div className="flex items-start flex-row justify-between p-3">
+          <h1>Watering</h1>
+          <Switch />
+        </div>
+      </div>
+      <div className="flex-col">
+        <div className="flex items-start flex-row justify-center p-3 gap-12">
+          <button
+            onClick={handleClick}
+            className={`bg-gradient-to-r ${gradientStart} ${gradientEnd} text-white px-12 py-5 rounded-md transition-all duration-500`}
+            style={{ transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)" }}
+          >
+            {buttonText}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Watering;
