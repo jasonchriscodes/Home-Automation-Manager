530fc91a98060a0b402d486f6bb22c909f6e47e5
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-04-12 03:27:24 +1200

    chore: add date time picker ui

--- frontend/src/components/Calendar.js
+++ frontend/src/components/Calendar.js
@@ -1,14 +1,55 @@
-import React from "react";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import React, { useRef, useState } from "react";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+import { StaticDateTimePicker } from "@mui/x-date-pickers/StaticDateTimePicker";
+import dayjs from "dayjs";
+import { DayCalendarSkeleton } from "@mui/x-date-pickers";
+import { Badge } from "@mui/material";
+import { PickersDay } from "@mui/x-date-pickers/PickersDay";
 
-const Calendar = () => {
+export default function StaticDateTimePickerLandscape() {
+  const initialValue = dayjs("2022-04-17");
+  const [isLoading, setIsLoading] = useState(false);
+  const [highlightedDays, setHighlightedDays] = useState([1, 16]);
+
+  function ServerDay(props) {
+    const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
+
+    const isSelected =
+      !props.outsideCurrentMonth &&
+      highlightedDays.indexOf(props.day.date()) > 0;
+
+    return (
+      <Badge
+        key={props.day.toString()}
+        overlap="circular"
+        badgeContent={isSelected ? "🌚" : undefined}
+      >
+        <PickersDay
+          {...other}
+          outsideCurrentMonth={outsideCurrentMonth}
+          day={day}
+        />
+      </Badge>
+    );
+  }
+
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <StaticDatePicker orientation="landscape" />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <StaticDateTimePicker
+        orientation="landscape"
+        defaultValue={initialValue}
+        loading={isLoading}
+        renderLoading={() => <DayCalendarSkeleton />}
+        slots={{
+          day: ServerDay,
+        }}
+        slotProps={{
+          day: {
+            highlightedDays,
+          },
+        }}
+      />
     </LocalizationProvider>
   );
-};
-
-export default Calendar;
+}
