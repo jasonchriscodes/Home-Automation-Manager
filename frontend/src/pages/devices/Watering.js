50b1dbc1bd4f05e8321668a4d80e7b4aa1c675b3
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-04-16 23:38:57 +1200

    chore: add refresh button

--- frontend/src/pages/devices/Watering.js
+++ frontend/src/pages/devices/Watering.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Switch from "./../../components/Switch";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSync } from "@fortawesome/free-solid-svg-icons";
 
 const Watering = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,12 +18,43 @@ const Watering = () => {
     ? "to-red-400 bg-pos-100"
     : "to-blue-400 bg-pos-100";
 
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  const handleSpinClick = () => {
+    setIsSpinning(true);
+    setTimeout(() => {
+      setIsSpinning(false);
+    }, 1500); // Change the timeout value to match refresh time
+  };
+
   return (
     <div className="p-4 shadow-md flex flex-wrap flex-col justify-between bg-white">
       <div className="flex-col">
         <div className="flex items-start flex-row justify-between p-3">
           <h1>Watering</h1>
           <Switch />
+        </div>
+      </div>
+      <div className="flex items-start flex-col justify-between p-3">
+        <button
+          className="text-gray-300 font-bold py-2 px-4 rounded"
+          onClick={handleSpinClick}
+          style={{ fontSize: 20 }}
+        >
+          {isSpinning ? (
+            <FontAwesomeIcon icon={faSync} spin className="animate-spin-fast" />
+          ) : (
+            <FontAwesomeIcon icon={faSync} />
+          )}{" "}
+        </button>
+        <h1>Bonsai</h1>
+        <h2>Watered on 15.04.2023</h2>
+        <div className="flex-col">
+          <div className="flex">
+            <div>water symbol </div>
+            <div>10%</div>
+          </div>
+          <h3>Humidity</h3>
         </div>
       </div>
       <div className="flex-col">
