d19c9a9e86e30aebacd177b3eca62a9fe646bb60
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-04-17 01:33:11 +1200

    chore: add plant status

--- frontend/src/pages/devices/Watering.js
+++ frontend/src/pages/devices/Watering.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import Switch from "./../../components/Switch";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSync } from "@fortawesome/free-solid-svg-icons";
+import { faDroplet } from "@fortawesome/free-solid-svg-icons";
+import { faTemperatureHalf } from "@fortawesome/free-solid-svg-icons";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
 
 const Watering = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -35,26 +38,67 @@ const Watering = () => {
           <Switch />
         </div>
       </div>
-      <div className="flex items-start flex-col justify-between p-3">
-        <button
-          className="text-gray-300 font-bold py-2 px-4 rounded"
-          onClick={handleSpinClick}
-          style={{ fontSize: 20 }}
-        >
-          {isSpinning ? (
-            <FontAwesomeIcon icon={faSync} spin className="animate-spin-fast" />
-          ) : (
-            <FontAwesomeIcon icon={faSync} />
-          )}{" "}
-        </button>
-        <h1>Bonsai</h1>
-        <h2>Watered on 15.04.2023</h2>
-        <div className="flex-col">
-          <div className="flex">
-            <div>water symbol </div>
-            <div>10%</div>
+      <div
+        style={{
+          backgroundImage: `url("https://res.cloudinary.com/jasoncloud13542/image/upload/v1681651538/bonsai_k3rw5g.png")`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          height: "600px",
+        }}
+      >
+        <div className="pl-3 pt-20">
+          <button
+            className="text-gray-300 font-bold rounded"
+            onClick={handleSpinClick}
+            style={{ fontSize: 20 }}
+          >
+            {isSpinning ? (
+              <FontAwesomeIcon
+                icon={faSync}
+                spin
+                className="animate-spin-fast"
+              />
+            ) : (
+              <FontAwesomeIcon icon={faSync} />
+            )}{" "}
+          </button>
+          <div className="pt-5 pb-5">
+            <p className="text-3xl font-bold">Bonsai</p>
+            <p className="text-md font-light">Watered on 15.04.2023</p>
           </div>
-          <h3>Humidity</h3>
+          <div className="flex-col py-2">
+            <div className="flex items-center">
+              <FontAwesomeIcon
+                icon={faDroplet}
+                style={{ fontSize: 18 }}
+                className="text-[#2563EB]"
+              />
+              <p className="text-xl px-2 font-bold">10%</p>
+            </div>
+            <p className="text-md font-light">Humidity</p>
+          </div>
+          <div className="flex-col py-2">
+            <div className="flex items-center">
+              <FontAwesomeIcon
+                icon={faTemperatureHalf}
+                style={{ fontSize: 18 }}
+                className="text-[#2563EB]"
+              />
+              <p className="text-xl px-2 font-bold">2°C</p>
+            </div>
+            <p className="text-md font-light">Temperature</p>
+          </div>
+          <div className="flex-col py-2">
+            <div className="flex items-center">
+              <FontAwesomeIcon
+                icon={faSun}
+                style={{ fontSize: 18 }}
+                className="text-[#2563EB]"
+              />
+              <p className="text-xl px-2 font-bold">70%</p>
+            </div>
+            <p className="text-md font-light">Light</p>
+          </div>
         </div>
       </div>
       <div className="flex-col">
