e1a6973ab02e614f00e4925e0746eb672dd06e8c
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-04-19 01:42:38 +1200

    chore: add mockDataExpenses.js

---
+++ frontend/src/data/mockData.js
@@ -0,0 +1,57 @@
+export const mockDataExpenses = [
+  {
+    id: 1,
+    date: "2023-04-19",
+    light: "310 W", // in W
+    curtain: "250 W",
+    door: "165 W",
+    watering: "359 W",
+    bin: "130 W",
+    totalElectricityPerDay: "1,214 W",
+    totalCostPerDay: "$277.8846", // daily charge $0.2289 /kwh
+  },
+  {
+    id: 2,
+    date: "2023-04-19",
+    light: "310 W", // in W
+    curtain: "250 W",
+    door: "165 W",
+    watering: "359 W",
+    bin: "130 W",
+    totalElectricityPerDay: "1,214 W",
+    totalCostPerDay: "$277.8846", // daily charge $0.2289 /kwh
+  },
+  {
+    id: 3,
+    date: "2023-04-19",
+    light: "310 W", // in W
+    curtain: "250 W",
+    door: "165 W",
+    watering: "359 W",
+    bin: "130 W",
+    totalElectricityPerDay: "1,214 W",
+    totalCostPerDay: "$277.8846", // daily charge $0.2289 /kwh
+  },
+  {
+    id: 4,
+    date: "2023-04-19",
+    light: "310 W", // in W
+    curtain: "250 W",
+    door: "165 W",
+    watering: "359 W",
+    bin: "130 W",
+    totalElectricityPerDay: "1,214 W",
+    totalCostPerDay: "$277.8846", // daily charge $0.2289 /kwh
+  },
+  {
+    id: 5,
+    date: "2023-04-19",
+    light: "310 W", // in W
+    curtain: "250 W",
+    door: "165 W",
+    watering: "359 W",
+    bin: "130 W",
+    totalElectricityPerDay: "1,214 W",
+    totalCostPerDay: "$277.8846", // daily charge $0.2289 /kwh
+  },
+];
