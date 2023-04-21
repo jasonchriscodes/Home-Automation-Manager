29cfefdfc5a8e455ffb5afff2c11ef0947ee64c4
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-04-22 10:18:52 +1200

    chore: revert devices room version

--- frontend/src/pages/rooms/LivingRoom.js
+++ frontend/src/pages/rooms/LivingRoom.js
@@ -3,49 +3,15 @@ import Bin from "../devices/Bin";
 import Curtain from "../devices/Curtain";
 import Door from "../devices/Door";
 import Watering from "../devices/Watering";
-import { Box } from "@mui/material";
 
 const Livingroom = () => {
   return (
-    <Box
-      display="grid"
-      gridTemplateColumns="repeat(12, 1fr)"
-      gridAutoRows="140px"
-      gap="20px"
-    >
-      <Box
-        gridColumn="span 3"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Watering />
-      </Box>
-      <Box
-        gridColumn="span 3"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Door />
-      </Box>
-      <Box
-        gridColumn="span 3"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Bin />
-      </Box>
-      <Box
-        gridColumn="span 3"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Curtain />
-      </Box>
-    </Box>
+    <div className="h-full grid grid-cols-1 xs:grid-cols-1 lg:grid-cols-2 pb-20">
+      <Watering />
+      <Door />
+      <Bin />
+      <Curtain />
+    </div>
   );
 };
 
--- frontend/src/pages/rooms/Office.js
+++ frontend/src/pages/rooms/Office.js
@@ -1,33 +1,13 @@
 import React from "react";
 import Curtain from "../devices/Curtain";
 import Light from "../devices/Light";
-import { Box } from "@mui/material";
 
 const Office = () => {
   return (
-    <Box
-      display="grid"
-      gridTemplateColumns="repeat(12, 1fr)"
-      gridAutoRows="140px"
-      gap="20px"
-    >
-      <Box
-        gridColumn="span 3"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Curtain />
-      </Box>
-      <Box
-        gridColumn="span 3"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Light />
-      </Box>
-    </Box>
+    <div className="h-full grid grid-cols-1 xs:grid-cols-1 lg:grid-cols-2 pb-20">
+      <Curtain />
+      <Light />
+    </div>
   );
 };
 
