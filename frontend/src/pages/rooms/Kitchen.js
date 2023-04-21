fa57cbde738e613630a8a9e5aa26587a3ca1da0e
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-04-22 10:15:17 +1200

    fix: horizontal scroller for other room

--- frontend/src/pages/rooms/Kitchen.js
+++ frontend/src/pages/rooms/Kitchen.js
@@ -1,13 +1,33 @@
 import React from "react";
 import Bin from "../devices/Bin";
 import Light from "../devices/Light";
+import { Box } from "@mui/material";
 
 const Kitchen = () => {
   return (
-    <div className="h-full grid grid-cols-1 xs:grid-cols-1 lg:grid-cols-2 pb-20">
-      <Light />
-      <Bin />
-    </div>
+    <Box
+      display="grid"
+      gridTemplateColumns="repeat(12, 1fr)"
+      gridAutoRows="140px"
+      gap="20px"
+    >
+      <Box
+        gridColumn="span 3"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Light />
+      </Box>
+      <Box
+        gridColumn="span 3"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Bin />
+      </Box>
+    </Box>
   );
 };
 
