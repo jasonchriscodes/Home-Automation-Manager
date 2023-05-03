e20ada5e5c0bb9ec728cfc4a537af459d0f5dc1e
oshMH <oshhettiarachchi@gmail.com>
2023-05-03 20:57:40 +1200

    add requirements.txt for virtual environment running

---
+++ hardware/python_code/python_code/02_watering_system.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as GPIO
+import time
+
+SENSOR_PIN = 17
+RELAY_PIN = 18
+REED_PIN = 22
+LED_PIN = 27
+IRO_PIN = 5
+LED_PIN_IR = 24
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(SENSOR_PIN, GPIO.IN)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+GPIO.setup(REED_PIN, GPIO.IN)
+GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(IRO_PIN, GPIO.IN)
+GPIO.setup(LED_PIN_IR, GPIO.OUT)
+
+try:
+    while True:
+        sensor_value = GPIO.input(SENSOR_PIN)
+        reed_value = GPIO.input(REED_PIN)
+        ir_val = GPIO.input(IRO_PIN)
+
+        if sensor_value == GPIO.HIGH:
+            GPIO.output(RELAY_PIN, GPIO.LOW)
+        else:
+            GPIO.output(RELAY_PIN, GPIO.HIGH)
+
+        if reed_value == GPIO.HIGH:
+            GPIO.output(LED_PIN, GPIO.HIGH)
+        else:
+            GPIO.output(LED_PIN, GPIO.LOW)
+
+        if ir_val == GPIO.HIGH:
+            GPIO.output(LED_PIN_IR, GPIO.HIGH)
+        else:
+            GPIO.output(LED_PIN_IR, GPIO.LOW)
+
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
---
+++ hardware/python_code/python_code/MongoDbConnection.py
@@ -0,0 +1,40 @@
+import pymongo
+import threading 
+import time 
+# set up the URI
+        
+def update_document(my_device, status):
+
+# create a client object to connect to the database
+    client = pymongo.MongoClient(uri)
+
+# access the database
+    db = client["Home_Automation_DB"]
+
+# access the collection in the database
+    collection = db["devices"]
+
+    collection.update_one({"deviceName": my_device }, {"$set": {"status": f'{status}'}})
+    print("Update Completed")
+
+def main():
+
+    update_document()
+
+if __name__ == "__main__":
+    main()
+
+
+#start the update thread 
+# update_thread = threading.Thread(target= update_document)    
+# update_thread.start()
+# # set up a Change Stream on the collection
+# with collection.watch() as stream:
+#     # listen for changes to the "status" field
+#     for event in stream:
+#         if ("updateDescription" in event
+#             and "updatedFields" in event["updateDescription"]
+#             and event["updateDescription"]["updatedFields"].get("status") in ["off", "on"]):
+#             print("Status changed to:", event["updateDescription"]["updatedFields"]["status"])
+
+	
