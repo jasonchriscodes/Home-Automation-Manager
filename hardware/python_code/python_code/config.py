a5ea4beae8e13bf04e94059c8acbfa3502b682e8
oshMH <oshhettiarachchi@gmail.com>
2023-05-06 20:08:46 +1200

    adding config.py with all pins and uri for db

---
+++ hardware/python_code/python_code/config.py
@@ -0,0 +1,19 @@
+# config.py
+SENSOR_PIN = 17
+RELAY_PIN = 18
+REED_PIN = 22
+LED_PIN = 27
+IRO_PIN = 5
+LED_PIN_IR = 24
+LIVINGROOM_LED = 23
+KITCHEN_LED = 25
+OFFICE_LED = 12
+
+MOTOR1_EN = 26
+MOTOR1_PIN1 = 19
+MOTOR1_PIN2 = 13
+MOTOR2_EN = 6
+MOTOR2_PIN1 = 21
+MOTOR2_PIN2 = 20
+
+uri = "mongodb+srv://Home_Automation_AUT:Home_Automation_AUT@cluster0.9awkbpi.mongodb.net/?retryWrites=true&w=majority"
